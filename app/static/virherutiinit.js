08a0adb3ebc81fa24d54ba25423c74eff6f0db67
60734740 <jukka.aula@kolumbus.fi>
2021-11-23 15:28:32 +0200

    Virheilmoitusten poisto lomakkeelta

--- app/static/virherutiinit.js
+++ app/static/virherutiinit.js
@@ -1,21 +1,20 @@
 
-function addkeyups(){
-var id;    
-document.querySelectorAll('.form-control').forEach(
-  e => {
-    let id = e.id;
-    console.log('addkeyups:'+id);
-    e.addEventListener('keyup', e => poistavirhe(id));
-    }
-  );
-}
+document.addEventListener("DOMContentLoaded", function(event) { 
+  document.querySelectorAll('.form-control').forEach(
+    e => {
+      let id = e.id;
+      //console.log('addkeyups:'+id);
+      e.addEventListener('keyup', e => poistavirhe(id));
+      }
+    );
+  });
 
     
-function poistavirhe(element_id){
-  console.log(`Täällä:${element_id}`);
+function poistavirhe(id){
+  //console.log(`Täällä:${element_id}`);
   document.querySelectorAll('.has-error').forEach(
     e => {
-      if (e.querySelector('#'+element_id)){   
+      if (e.querySelector('#'+id)){   
         e.querySelector('.help-block').remove();
         e.classList.remove('has-error');
         }
